3a9b91db7a135f83cd96212b600dddf51613e8fe
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2021-07-16 11:37:44 +0200

    :sparkles: Detect new items

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('Hello world')
+const fetch = require('node-fetch');
+
+const items =  [];
+const postCMCDaily = async () => {
+  const response = await fetch('https://cmc-daily-api.wouterdeschuyter.be');
+  const data = await response.json();
+
+  if (items.length === 0) {
+    items.push(...data);
+  }
+
+  for (const item of data) {
+    if (!items.includes(item)) {
+      console.log(`New article: ${item}`);
+      items.push(item);
+    }
+  }
+};
+
+(async () => {
+  console.log('Slack bot is running!');
+  await postCMCDaily();
+})();
